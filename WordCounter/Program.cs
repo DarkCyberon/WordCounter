eee04983997906af1f8ff0c7f5324ee8a768e529
DarkCyberon <Xcom.D@yandex.ru>
2023-11-15 11:39:44 +0500

    Came into Git

---
+++ WordCounter/Program.cs
@@ -0,0 +1,14 @@
+﻿using com.sun.tools.javadoc;
+using PorterStemmer.Stemmers;
+using WordCounter;
+
+class Program
+{
+    static void Main()
+    {
+        //TimeMeasurer tm = new TimeMeasurer();
+        //tm.Start(5000);
+        InputOutput io = new InputOutput();
+        io.ConsoleOut(io.ReadFile());
+    }
+}
